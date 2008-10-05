0c54ed33fe9a0eeae804f1b5dc9bb94b6cc4df3e
jasonwhaley <jasonwhaley@5f254492-7750-0410-8644-a53f82ddc3df>
2008-10-05 01:45:56 +0000

    REMOVING: Old Files for ScpFacade ADDED: ScpDownload task
    git-svn-id: http://securechannelfacade.googlecode.com/svn/trunk@14 5f254492-7750-0410-8644-a53f82ddc3df

--- src/main/java/org/rev6/scf/ScpException.java
+++
@@ -1,9 +0,0 @@
-package org.rev6.scf;
-
-public class ScpException extends Exception
-{
-  static final long serialVersionUID = 1L;
-  public ScpException(String message){super(message);}
-  public ScpException(Throwable cause){super(cause);}
-  public ScpException(String message, Throwable cause){super(message, cause);}
-}
